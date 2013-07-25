c3cdf9f9b6afc7bee166a03b4406a3803ab40ea3
Steven Watanabe <steven@providere-consulting.com>
2013-07-25 11:01:41 -0400

    Translate upc_notify

--- Transform.cpp
+++ Transform.cpp
@@ -1,5 +1,6 @@
 #include <clang/Tooling/Tooling.h>
 #include <clang/Sema/SemaConsumer.h>
+#include <clang/Sema/Scope.h>
 #include <clang/AST/Stmt.h>
 #include <clang/AST/ASTContext.h>
 #include <clang/AST/Decl.h>
@@ -18,10 +19,17 @@ namespace {
 
   struct UPCRDecls {
     FunctionDecl * upcr_notify;
-    UPCRDecls(ASTContext& Context, DeclContext *DC) {
+    explicit UPCRDecls(ASTContext& Context) {
+      DeclContext *DC = Context.getTranslationUnitDecl();
       QualType argTypes[] = { Context.IntTy, Context.IntTy };
       QualType Ty = Context.getFunctionType(Context.VoidTy, argTypes, 2, FunctionProtoType::ExtProtoInfo());
       upcr_notify = FunctionDecl::Create(Context, DC, SourceLocation(), SourceLocation(), DeclarationName(&Context.Idents.get("upcr_notify")), Ty, Context.getTrivialTypeSourceInfo(Ty));
+      llvm::SmallVector<ParmVarDecl *, 2> Params;
+      Params.push_back(ParmVarDecl::Create(Context, upcr_notify, SourceLocation(), SourceLocation(), 0, Context.IntTy, 0, SC_None, SC_None, 0));
+      Params.push_back(ParmVarDecl::Create(Context, upcr_notify, SourceLocation(), SourceLocation(), 0, Context.IntTy, 0, SC_None, SC_None, 0));
+      Params[0]->setScopeInfo(0, 0);
+      Params[0]->setScopeInfo(0, 1);
+      upcr_notify->setParams(Params);
     }
   };
 
@@ -29,6 +37,10 @@ namespace {
   public:
     RemoveUPCTransform(Sema& S, UPCRDecls* D) : TreeTransform(S), Decls(D) {
       AlwaysRebuild();
+    }
+    // TreeTransform ignores AlwayRebuild for literals
+    ExprResult TransformIntegerLiteral(IntegerLiteral *E) {
+      return IntegerLiteral::Create(SemaRef.Context, E->getValue(), E->getType(), E->getLocation());
     }
     ExprResult BuildUPCRCall(FunctionDecl *FD, std::vector<Expr*>& args) {
       ExprResult Fn = SemaRef.BuildDeclRefExpr(FD, FD->getType(), VK_LValue, SourceLocation());
@@ -53,15 +65,44 @@ namespace {
     Decl *TransformDecl(SourceLocation Loc, Decl *D) {
       return TreeTransform::TransformDecl(Loc, D);
     }
-    Decl *TransformDefinition(SourceLocation Loc, Decl *D) {
+    Decl *TransformDeclaration(Decl *D, DeclContext *DC) {
       if(TranslationUnitDecl *TUD = dyn_cast<TranslationUnitDecl>(D)) {
 	return TransformTranslationUnitDecl(TUD);
+      } else if(FunctionDecl *FD = dyn_cast<FunctionDecl>(D)) {
+	FunctionDecl *result = FunctionDecl::Create(SemaRef.Context, DC, FD->getLocStart(),
+				    FD->getNameInfo(), TransformType(FD->getType()),
+				    TransformType(FD->getTypeSourceInfo()),
+				    FD->getStorageClass(), FD->getStorageClassAsWritten(),
+				    FD->isInlineSpecified(), FD->hasWrittenPrototype(),
+				    FD->isConstexpr());
+	if(FD->doesThisDeclarationHaveABody()) {
+	  SemaRef.ActOnStartOfFunctionDef(0, result);
+	  Sema::ContextRAII savedContext(SemaRef, result);
+	  SemaRef.ActOnFinishFunctionBody(result, TransformStmt(FD->getBody()).get());
+	}
+	return result;
+      } else if(VarDecl *VD = dyn_cast<VarDecl>(D)) {
+	VarDecl *result = VarDecl::Create(SemaRef.Context, DC, VD->getLocStart(), VD->getLocation(), VD->getIdentifier(),
+			       TransformType(VD->getType()), TransformType(VD->getTypeSourceInfo()),
+			       VD->getStorageClass(), VD->getStorageClassAsWritten());
+	if(Expr *Init = VD->getInit()) {
+	  result->setInit(TransformExpr(Init).get());
+	}
+	return result;
+      } else if(RecordDecl *RD = dyn_cast<RecordDecl>(D)) {
+	return RecordDecl::Create(SemaRef.Context, RD->getTagKind(), DC,
+				  RD->getLocStart(), RD->getLocation(),
+				  RD->getIdentifier(), cast_or_null<RecordDecl>(TransformDecl(SourceLocation(), RD->getPreviousDecl())));
+      } else if(TypedefDecl *TD = dyn_cast<TypedefDecl>(D)) {
+	return TypedefDecl::Create(SemaRef.Context, DC, TD->getLocStart(), TD->getLocation(), TD->getIdentifier(), TransformType(TD->getTypeSourceInfo()));
       } else {
-	return TreeTransform::TransformDefinition(Loc, D);
+	assert(!"Unknown Decl");
       }
     }
     Decl *TransformTranslationUnitDecl(TranslationUnitDecl *D) {
-      TranslationUnitDecl *result = TranslationUnitDecl::Create(SemaRef.Context);
+      TranslationUnitDecl *result = SemaRef.Context.getTranslationUnitDecl();
+      Scope CurScope(0, Scope::DeclScope, SemaRef.getDiagnostics());
+      SemaRef.PushDeclContext(&CurScope, result);
       CopyDeclContext(D, result);
       return result;
     }
@@ -69,7 +110,7 @@ namespace {
     void CopyDeclContext(DeclContext *Src, DeclContext *Dst) {
       for(DeclContext::decl_iterator iter = Src->decls_begin(),
           end = Src->decls_end(); iter != end; ++iter) {
-	Dst->addDecl(TransformDefinition(SourceLocation(), *iter));
+	Dst->addDecl(TransformDeclaration(*iter, Dst));
       }
     }
   };
@@ -79,9 +120,16 @@ namespace {
     RemoveUPCConsumer() : filename("upc.c") {}
     virtual void HandleTranslationUnit(clang::ASTContext &Context) {
       TranslationUnitDecl *top = Context.getTranslationUnitDecl();
-      UPCRDecls Decls(Context, top);
-      RemoveUPCTransform Trans(*S, &Decls);
-      Decl *Result = top;
+      // Copy the ASTContext and Sema
+      LangOptions LangOpts = Context.getLangOpts();
+      ASTContext newContext(LangOpts, Context.getSourceManager(), &Context.getTargetInfo(),
+			    Context.Idents, Context.Selectors, Context.BuiltinInfo,
+			    Context.getTypes().size());
+      ASTConsumer nullConsumer;
+      UPCRDecls Decls(newContext);
+      Sema newSema(S->getPreprocessor(), newContext, nullConsumer);
+      RemoveUPCTransform Trans(newSema, &Decls);
+      Decl *Result = Trans.TransformTranslationUnitDecl(top);
       std::string error;
       llvm::raw_fd_ostream OS(filename.c_str(), error);
       Result->print(OS);
